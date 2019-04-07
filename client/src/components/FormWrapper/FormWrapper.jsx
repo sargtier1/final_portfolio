714823114f29ffec35852002a0ff3c5c169385da
JustTams <sarg.tier1@gmail.com>
2019-04-07 18:52:41 -0500

    validation

--- client/src/components/FormWrapper/FormWrapper.jsx
+++ client/src/components/FormWrapper/FormWrapper.jsx
@@ -16,9 +16,11 @@ class FormWrapper extends Component {
       firstName: "",
       lastName: "",
       email: "",
+      business: "",
       phoneNumber: "",
       comments: "",
-      formSuccess: false
+      formSuccess: false,
+      formMissing: false
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -26,39 +28,124 @@ class FormWrapper extends Component {
 
   handleChange = (
     e,
-    { firstName, lastName, email, phoneNumber, comments, value }
+    { firstName, lastName, email, phoneNumber, business, comments, value }
   ) => {
-    e.preventDefault()
+    e.preventDefault();
     this.setState({
       [firstName]: value,
       [lastName]: value,
       [email]: value,
       [phoneNumber]: value,
+      [business]: value,
       [comments]: value
     });
   };
 
   handleSubmit = e => {
     e.preventDefault();
+    console.log(this.state)
     this.setState({ formSuccess: true });
-
   };
+
+  formValidation = () => {
+    // display error message for missing fields
+    if (
+      this.state.firstName === "" ||
+      this.state.lastName === "" ||
+      this.state.email === "" ||
+      this.state.phoneNumber === "" ||
+      this.state.business === "" ||
+      this.state.comments === ""
+    ) {
+      this.setState({ 
+        formMissing: true
+       })
+       return
+    } 
+    // Name Check
+
+    if (this.state.firstName === "") {
+      this.setState({ 
+        formMissing: true
+       })
+       return true
+    }
+    if (this.state.firstName === "") {
+      this.setState({ 
+        formMissing: true
+       })
+       return true
+    }
+    //  Email Check
+    const characterCheck = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const isEmail = characterCheck.test(String(this.state.email).toLowerCase())
+    if (!isEmail) {
+      this.setState({
+        formMissing: true
+      })
+      return true
+    }
+    // Phonen Number Check
+    if (isNaN(this.state.phoneNumber)) {
+      this.setState({
+        formMissing: true
+      })
+      return true
+    }
+    // Business affiars Check
+    if (this.state.business === "") {
+      this.setState({
+        formMissing: true
+      })
+      return true
+    }
+    // Comments Check
+    if (this.state.comments === "") {
+      this.setState({
+        formMissing: true
+      })
+      return true
+    }
+  }
 
   render() {
     return (
       <React.Fragment>
         <Form onSubmit={this.handleSubmit} success={this.state.formSuccess}>
           <Form.Group>
-            <Form.Input label="First name" placeholder="First Name" width={8} />
-            <Form.Input label="Last Name" placeholder="Last Name" width={8} />
+            <Form.Input
+              label="First name"
+              placeholder="First Name"
+              width={8}
+              onChange={e => this.setState({ firstName: e.target.value })}
+              error={this.state.formMissing}
+            />
+            <Form.Input
+              label="Last Name"
+              placeholder="Last Name"
+              width={8}
+              onChange={e => this.setState({ lastName: e.target.value })}
+              error={this.state.formMissing}
+
+            />
           </Form.Group>
           <Form.Group>
             <Form.Input
               label="E-Mail"
               placeholder="whatevs@something.com"
               width={8}
+              onChange={e => this.setState({ email: e.target.value })}
+              error={this.state.formMissing}
+
             />
-            <Form.Input label="Phone" placeholder="(111) 111-1111" width={8} />
+            <Form.Input
+              label="Phone"
+              placeholder="(111) 111-1111"
+              width={8}
+              onChange={e => this.setState({ phoneNumber: e.target.value })}
+              error={this.state.formMissing}
+
+            />
           </Form.Group>
           <Form.Group>
             <Form.Select
@@ -66,6 +153,9 @@ class FormWrapper extends Component {
               options={options}
               placeholder="Please Choose One"
               width={16}
+              onChange={e => this.setState({ business: this.options })}
+              error={this.state.formMissing}
+
             />
           </Form.Group>
           <Form.Group>
@@ -75,6 +165,8 @@ class FormWrapper extends Component {
               label="Additional Comments"
               placeholder="Please Note Here."
               width={16}
+              onChange={e => this.setState({ comments: e.target.value })}
+              error={this.state.formMissing}
             />
           </Form.Group>
           <Message
